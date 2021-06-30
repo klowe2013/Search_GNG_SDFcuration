766a820745173490ff67b636f671f1b3d12f13f9
Kaleb Lowe <kalebalowe91@gmail.com>
2021-06-30 10:52:00 -0400

    Clicks were getting lost with the reorganization where placeholders are put before the data - changed to Promise which gets add to the window.onload initializer

--- static/js/single_sst.js
+++ static/js/single_sst.js
@@ -1,5 +1,5 @@
 // Manage NHP list
-function getNHPs(){
+function getNHPs() {
     $.getJSON({
         url: "/nhp-update-cb", success: (res) => {
             updateNHPList(res.nhpVals, res.nhpLabels);
@@ -7,11 +7,11 @@ function getNHPs(){
     })
 }
 
-function updateNHPList(vals, labels){
+function updateNHPList(vals, labels) {
     // Start by removing all options
     const nhpOpts = document.querySelectorAll('#nhp-dropdown option');
     nhpOpts.forEach(o => o.remove());
-    
+
     const nhpDropDown = document.getElementById('nhp-dropdown');
     var i;
     for (i = 0; i < vals.length; i++) {
@@ -24,21 +24,21 @@ function updateNHPList(vals, labels){
 }
 
 // Manage session list
-function getSessList(nhp){
+function getSessList(nhp) {
     // Request from backend by sending nhp value
     $.getJSON({
-        url: "/sess-update-cb", data: {'nhp': nhp}, success: (res) => {
+        url: "/sess-update-cb", data: { 'nhp': nhp }, success: (res) => {
             updateSessList(res.sessList);
         }
     })
 }
 
 // Update session list in the DOM
-function updateSessList(sessList){
+function updateSessList(sessList) {
     // Start by removing all options
     const sessOpts = document.querySelectorAll('#sess-dropdown option');
     sessOpts.forEach(o => o.remove());
-    
+
     const sessDropDown = document.getElementById('sess-dropdown');
     var i;
     for (i = 0; i < sessList.length; i++) {
@@ -51,21 +51,21 @@ function updateSessList(sessList){
 }
 
 // Get the unit list from a given session
-function getUnitList(session){
+function getUnitList(session) {
     // Request from backend by sending session value
     $.getJSON({
-        url: "/unit-update-cb", data: {'sess': session}, success: (res) => {
+        url: "/unit-update-cb", data: { 'sess': session }, success: (res) => {
             updateUnitList(res.unitList);
         }
     })
 }
 
 // Update unit dropdown in the DOM
-function updateUnitList(unitList){
+function updateUnitList(unitList) {
     // Start by removing all options
     const unitOpts = document.querySelectorAll('#unit-dropdown option');
     unitOpts.forEach(o => o.remove());
-    
+
     const unitDropDown = document.getElementById('unit-dropdown');
     var i;
     for (i = 0; i < unitList.length; i++) {
@@ -77,7 +77,7 @@ function updateUnitList(unitList){
 }
 
 // Hide unchecked values
-function hideUnchecked(){
+function hideUnchecked() {
     console.log('Entering hideUnchecked')
     // Get checked elements
     const inGoCheck = document.getElementById("in-go").checked;
@@ -92,105 +92,108 @@ function hideUnchecked(){
         let thisPlot;
         let visIndices = []
         let invisIndices = []
-        for (ia=0; ia < arrNames.length; ia++){
+        for (ia = 0; ia < arrNames.length; ia++) {
             thisPlot = document.getElementById(arrNames[ia]);
             visIndices = []
             invisIndices = []
             // Loop through children of this plot
-            for (i=0; i < thisPlot.data.length; i++){
-                if (Object.keys(thisPlot.data[i]).includes('type')){
+            for (i = 0; i < thisPlot.data.length; i++) {
+                if (Object.keys(thisPlot.data[i]).includes('type')) {
                     // Check if this is "out-go"
-                    if (thisPlot.data[i].name[1] !== '0' && thisPlot.data[i].name[3] === 'O'){
+                    if (thisPlot.data[i].name[1] !== '0' && thisPlot.data[i].name[3] === 'O') {
                         // Check if "out-go" is checked
-                        if (outGoCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                        if (outGoCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             visIndices.push(i)
-                        } else if (!outGoCheck && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                        } else if (!outGoCheck && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             invisIndices.push(i)
                         }
                     }
                     // Check if this is "in-no-go"
-                    if (thisPlot.data[i].name[1] === '0' && thisPlot.data[i].name[3] === 'I'){
+                    if (thisPlot.data[i].name[1] === '0' && thisPlot.data[i].name[3] === 'I') {
                         // Check if "out-go" is checked
-                        if (inNgCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                        if (inNgCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             visIndices.push(i)
-                        } else if (!inNgCheck && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                        } else if (!inNgCheck && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             invisIndices.push(i)
                         }
                     }
                     // Check if this is "out-no-go"
-                    if (thisPlot.data[i].name[1] === '0' && thisPlot.data[i].name[3] === 'O'){
+                    if (thisPlot.data[i].name[1] === '0' && thisPlot.data[i].name[3] === 'O') {
                         // Check if "out-go" is checked
-                        if (outNgCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                        if (outNgCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             visIndices.push(i)
-                        } else if (!outNgCheck && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                        } else if (!outNgCheck && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             invisIndices.push(i)
                         }
                     }
                     // If name is "sst", check if both "in-go" and "out-go" are checked
-                    if (thisPlot.data[i].name.slice(0,3) === 'sst'){
-                        if (inGoCheck && outGoCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                    if (thisPlot.data[i].name.slice(0, 3) === 'sst') {
+                        if (inGoCheck && outGoCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             visIndices.push(i)
-                        } else if ((!inGoCheck || !outGoCheck ) && (thisPlot.data[i].visible ||  typeof thisPlot.data[i].visible === 'undefined')){
+                        } else if ((!inGoCheck || !outGoCheck) && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             invisIndices.push(i)
                         }
                     }
                     // If name is "cdt", check if both "in-go" and "in-nogo" are checked
-                    if (thisPlot.data[i].name.slice(0,3) === 'cdt'){
-                        if (inGoCheck && inNgCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')){
+                    if (thisPlot.data[i].name.slice(0, 3) === 'cdt') {
+                        if (inGoCheck && inNgCheck && (!thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             visIndices.push(i)
-                        } else if ((!inGoCheck || !inNgCheck ) && (thisPlot.data[i].visible ||  typeof thisPlot.data[i].visible === 'undefined')){
+                        } else if ((!inGoCheck || !inNgCheck) && (thisPlot.data[i].visible || typeof thisPlot.data[i].visible === 'undefined')) {
                             invisIndices.push(i)
                         }
                     }
                 }
             }
-            if (visIndices.length > 0){
-                Plotly.restyle(thisPlot, {'visible': true}, visIndices);
+            if (visIndices.length > 0) {
+                Plotly.restyle(thisPlot, { 'visible': true }, visIndices);
             }
-            if (invisIndices.length > 0){
-                Plotly.restyle(thisPlot, {'visible': false}, invisIndices);
+            if (invisIndices.length > 0) {
+                Plotly.restyle(thisPlot, { 'visible': false }, invisIndices);
             }
         }
         resolve();
-    })    
+    })
 }
 
-function updateClickEvents(){
-    let allPlots = {};
-    var i;
-    for (i=0; i< arrNames.length; i++){
-        allPlots[arrNames[i]] = document.getElementById(arrNames[i]);
-        // Initialize on-click listeners
-        allPlots[arrNames[i]].on('plotly_click', (data, i) =>{
-            // Get the X value
-            const thisX = data['points'][0]['x'];
-            // Get this element
-            const thisPlot = data['event']['path'][6]['id'];
-            // Try to infer what type of selection point this is
-            const selType = inferSelection();
-            // Send to sst-click
-            $.getJSON({
-                url: "/sst-click-cb", data: {'x': thisX, 'plotID': thisPlot, 'selType': selType}, success: (res) => {
-                    // Get the layout of thisPlot
-                    let thisY = document.getElementById(thisPlot).layout.yaxis.range;
-                    Plotly.addTraces(document.getElementById(thisPlot), {x: [thisX, thisX], y: thisY, line: {'color': selTypeColors[selType]}});
-                    Plotly.relayout(document.getElementById(thisPlot), {'yaxis.range': thisY});
-                }
+function updateClickEvents() {
+    return new Promise((resolve, reject) => {
+        let allPlots = {};
+        var i;
+        for (i = 0; i < arrNames.length; i++) {
+            allPlots[arrNames[i]] = document.getElementById(arrNames[i]);
+            // Initialize on-click listeners
+            allPlots[arrNames[i]].on('plotly_click', (data, i) => {
+                // Get the X value
+                const thisX = data['points'][0]['x'];
+                // Get this element
+                const thisPlot = data['event']['path'][6]['id'];
+                // Try to infer what type of selection point this is
+                const selType = inferSelection();
+                // Send to sst-click
+                $.getJSON({
+                    url: "/sst-click-cb", data: { 'x': thisX, 'plotID': thisPlot, 'selType': selType }, success: (res) => {
+                        // Get the layout of thisPlot
+                        let thisY = document.getElementById(thisPlot).layout.yaxis.range;
+                        Plotly.addTraces(document.getElementById(thisPlot), { x: [thisX, thisX], y: thisY, line: { 'color': selTypeColors[selType] } });
+                        Plotly.relayout(document.getElementById(thisPlot), { 'yaxis.range': thisY });
+                    }
+                })
             })
-        })
-    }
+        }
+        resolve();
+    })
 }
 
-function submitSSTs(){
+function submitSSTs() {
     $.getJSON({
-        url: "/sst-submit", data: {'submit': true}, success: (res) => {
+        url: "/sst-submit", data: { 'submit': true }, success: (res) => {
             console.log('Successfully submitted SST')
         }
     })
 }
 
 // When unit changes we don't want to automatically reload, but when "Update" is sent we do want to re-pull the plots
-function updateSessPlots(forcePull){
+function updateSessPlots(forcePull) {
     // Get the current state of the session and units (NHP isn't necessary because it's redundant with session)
     const sessValue = document.getElementById('sess-dropdown').value
     const unitValue = document.getElementById('unit-dropdown').value
@@ -199,17 +202,17 @@ function updateSessPlots(forcePull){
     const sMinX = document.getElementById('sacc_xmin_state').value
     const sMaxX = document.getElementById('sacc_xmax_state').value
 
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         updateButton.value = 'Requesting...'
         $.getJSON({
-            url: "/plot-update-cb", data: {'sess': sessValue, 'unit': unitValue, 'aMinX': aMinX, 'aMaxX': aMaxX, 'sMinX': sMinX, 'sMaxX': sMaxX, 'forcePull': forcePull}, success: (res) => {
+            url: "/plot-update-cb", data: { 'sess': sessValue, 'unit': unitValue, 'aMinX': aMinX, 'aMaxX': aMaxX, 'sMinX': sMinX, 'sMaxX': sMaxX, 'forcePull': forcePull }, success: (res) => {
                 var keys = Object.keys(res);
                 console.log(res)
                 // If the session and unit are the same (i.e., we're just updating axis range and check conditions), res returns {'refresh': False}
-                if (keys[0]==='refresh'){
+                if (keys[0] === 'refresh') {
                     updateButton.value = 'Fixing Axes...'
-                    let arrUpdate = {'xaxis.range': [aMinX, aMaxX]};
-                    let saccUpdate = {'xaxis.range': [sMinX, sMaxX]};
+                    let arrUpdate = { 'xaxis.range': [aMinX, aMaxX] };
+                    let saccUpdate = { 'xaxis.range': [sMinX, sMaxX] };
                     const arrPlots = Array.from(document.getElementsByClassName('arr-plot'))
                     arrPlots.forEach((item) => {
                         Plotly.relayout(item, arrUpdate);
@@ -222,21 +225,21 @@ function updateSessPlots(forcePull){
                 } else {
                     updateButton.value = 'Parsing Input...';
                     var i;
-                    for (i=0; i < keys.length; i++){
+                    for (i = 0; i < keys.length; i++) {
                         var arrGraphs = JSON.parse(res[keys[i]].array.data);
                         var saccGraphs = JSON.parse(res[keys[i]].saccade.data);
-                        Plotly.react(res[keys[i]].array.id, arrGraphs, {});  
-                        Plotly.react(res[keys[i]].saccade.id, saccGraphs, {});  
+                        Plotly.react(res[keys[i]].array.id, arrGraphs, {});
+                        Plotly.react(res[keys[i]].saccade.id, saccGraphs, {});
                     }
                     resolve();
                 }
             }
         })
     })
-    
+
 }
 
-function inferSelection(){
+function inferSelection() {
     const inGoChecked = document.getElementById('in-go').checked;
     const outGoChecked = document.getElementById('out-go').checked;
     const inNgChecked = document.getElementById('in-ng').checked;
@@ -244,29 +247,29 @@ function inferSelection(){
 
     let selType = '';
     // Basic two options are SST and CDT
-    if (inGoChecked && outGoChecked && !inNgChecked){
+    if (inGoChecked && outGoChecked && !inNgChecked) {
         selType = 'sst';
-    } else if (inGoChecked && !outGoChecked && inNgChecked){
+    } else if (inGoChecked && !outGoChecked && inNgChecked) {
         selType = 'cdt';
-    } else if (!inGoChecked && !outGoChecked && inNgChecked && outNgChecked){
+    } else if (!inGoChecked && !outGoChecked && inNgChecked && outNgChecked) {
         selType = 'ngsst';
-    } else if (!inGoChecked && outGoChecked && !inNgChecked && outNgChecked){
+    } else if (!inGoChecked && outGoChecked && !inNgChecked && outNgChecked) {
         selType = 'ocdt';
-    } else{
+    } else {
         selType = 'unk';
     }
     return selType;
 }
 
-function clearSSTs(){
+function clearSSTs() {
     // Later we'll want to make this more dynamic (give options to clear...), but for now resetting all will work
     const sessValue = document.getElementById('sess-dropdown').value
     const unitValue = document.getElementById('unit-dropdown').value
-    
+
     document.getElementById('sst_clear').value = 'Clearing SSTs...'
-    
+
     $.getJSON({
-        url: "/clear-sst-cb", data: {'sess': sessValue, 'unit': unitValue}, success: (res) => {
+        url: "/clear-sst-cb", data: { 'sess': sessValue, 'unit': unitValue }, success: (res) => {
             document.getElementById('sst_clear').value = 'Clear Selection Times'
             const arrPlots = Array.from(document.getElementsByClassName('arr-plot'))
             const saccPlots = Array.from(document.getElementsByClassName('sacc-plot'))
@@ -276,12 +279,12 @@ function clearSSTs(){
                 // Pull line names from data
                 let names = itemData.map((line) => line.name);
                 let delTraces = []
-                names.forEach((o,i) => {
-                    if (selTypes.includes(o.slice(0,3))){
+                names.forEach((o, i) => {
+                    if (selTypes.includes(o.slice(0, 3))) {
                         delTraces.push(i)
                     }
                 })
-                Plotly.deleteTraces(item,delTraces)
+                Plotly.deleteTraces(item, delTraces)
             })
             saccPlots.forEach((item) => {
                 // Item.data contains lines
@@ -289,20 +292,20 @@ function clearSSTs(){
                 // Pull line names from data
                 let names = itemData.map((line) => line.name);
                 let delTraces = []
-                names.forEach((o,i) => {
-                    if (selTypes.includes(o.slice(0,3))){
+                names.forEach((o, i) => {
+                    if (selTypes.includes(o.slice(0, 3))) {
                         delTraces.push(i)
                     }
                 })
-                Plotly.deleteTraces(item,delTraces)
+                Plotly.deleteTraces(item, delTraces)
             })
         }
     })
 }
 
-function logout(){
+function logout() {
     $.getJSON({
-        url: "/login-cb", data: {'buttonState': 'Logout'}, success: () => {
+        url: "/login-cb", data: { 'buttonState': 'Logout' }, success: () => {
             window.location.href = "/login";
         }
     })
@@ -310,15 +313,17 @@ function logout(){
 
 // Attach update function to page load
 window.onload = () => {
-    updateSessPlots(true).then(()=>{
-        hideUnchecked().then(() => updateButton.value = 'Update')
+    updateSessPlots(true).then(() => {
+        updateClickEvents().then(() => {
+            hideUnchecked().then(() => updateButton.value = 'Update')
+        })
     })
 }
 
 // Set global variables
-const arrNames = ["hh-array", "hl-array", "lh-array", "ll-array","hh-sacc", "hl-sacc", "lh-sacc", "ll-sacc"];
-const selTypeColors = {'sst': 'rgb(54,201,54)', 'cdt': 'rgb(54, 201, 201)', 'ngsst': 'rgb(230, 147, 23)', 'ocdt': 'rgb(201, 201, 54)', 'unk': 'rgb(0,0,0)'};
-const selTypes = ['sst','cdt','ngs','ocd','unk']
+const arrNames = ["hh-array", "hl-array", "lh-array", "ll-array", "hh-sacc", "hl-sacc", "lh-sacc", "ll-sacc"];
+const selTypeColors = { 'sst': 'rgb(54,201,54)', 'cdt': 'rgb(54, 201, 201)', 'ngsst': 'rgb(230, 147, 23)', 'ocdt': 'rgb(201, 201, 54)', 'unk': 'rgb(0,0,0)' };
+const selTypes = ['sst', 'cdt', 'ngs', 'ocd', 'unk']
 
 // Add event listeners for NHP and Session dropdowns
 const nhpElement = document.getElementById('nhp-dropdown')
@@ -331,10 +336,10 @@ sessElement.addEventListener("change", (e) => {
     getUnitList(e.target.value)
 })
 updateButton.addEventListener('click', () => {
-    updateSessPlots(false).then(()=>{
+    updateSessPlots(false).then(() => {
         hideUnchecked().then(() => updateButton.value = 'Update')
     })
-})    
+})
 
 // Hide initially unchecked values
-updateClickEvents();
+//updateClickEvents();
