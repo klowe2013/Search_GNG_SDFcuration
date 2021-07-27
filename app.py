f5b2b198a5b145198096cada3e5d361f960ae974
Kaleb Lowe <kalebalowe91@gmail.com>
2021-07-27 15:58:41 -0400

    Logic of Update callback overwrites SSTs with output from SpikesFromDB, so reopening a unit overwrites any changes. Now fixed by adding session/unit combo to session dict to check whether it's been seen already

--- app.py
+++ app.py
@@ -43,25 +43,27 @@ sst_dict = {}
 ##############################
 
 # Route for landing page
+
+
 @app.route('/')
 def landing():
     # Check if logged in, otherwise redirect to login page
     is_auth = session.get('is_auth')
     if is_auth is False or is_auth is None:
         return redirect(url_for('login_page'))
-    
+
     return render_template('home.html',
                            user=session['user'])
 
 
 # Route for login page
-@app.route('/login', methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_page():
     is_auth = session.get('is_auth')
     if is_auth:
         return redirect(url_for('landing'))
-    return render_template('login.html')    
-        
+    return render_template('login.html')
+
 
 # Route for checking individual SSTs
 @app.route('/single-sst')
@@ -70,52 +72,55 @@ def single_sst():
     is_auth = session.get('is_auth')
     if is_auth is False or is_auth is None:
         session['user'] = 'none'
-    
+
     # Make sst_dict a session variable
     session['sst_dict'] = sst_dict
-            
+    session['units_loaded'] = []
+
     # Make list of NHPs
     nhp_list = PullNHPs(sdf_coll, session['is_auth'])
     sess_list = PullSess(sdf_coll, nhp_list[0], session['is_auth'])
     unit_list = PullUnits(sdf_coll, sess_list[0], session['is_auth'])
 
-    # Load initial spike/SDF values            
+    # Load initial spike/SDF values
     #v_dict, v_dict_sem, m_dict, m_dict_sem, unit_ssts = SpikesFromDB(sess_list[0], unit_list[0], sdf_coll, user=session['user'])
-    
+
     # Make array-aligned figures
     for ic, cond in enumerate(plot_conds):
-        my_figs[cond]['array']['data'] = json.dumps(go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
+        my_figs[cond]['array']['data'] = json.dumps(
+            go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
         my_figs[cond]['array']['id'] = '{}-array'.format(cond)
-        my_figs[cond]['saccade']['data'] = json.dumps(go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
+        my_figs[cond]['saccade']['data'] = json.dumps(
+            go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
         my_figs[cond]['saccade']['id'] = '{}-sacc'.format(cond)
-        
-        
-    return render_template('single_sst.html', 
-                           cond_plots = my_figs,
-                           nhps = nhp_list,
-                           sessions = sess_list,
-                           units = unit_list,
-                           logged_in = session['is_auth'])
-    
+
+    return render_template('single_sst.html',
+                           cond_plots=my_figs,
+                           nhps=nhp_list,
+                           sessions=sess_list,
+                           units=unit_list,
+                           logged_in=session['is_auth'])
+
 
 # Page for looking at group averages
 @app.route('/group-averages')
 def group_averages():
     # First, we need to pull the units
     # pop_sdfs, unit_nhps = AllSDFs(sdf_coll, session['is_auth'])
-    
+
     # Now generate the plots
     for ic, cond in enumerate(plot_conds):
-        my_figs[cond]['array']['data'] = json.dumps(go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
+        my_figs[cond]['array']['data'] = json.dumps(
+            go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
         my_figs[cond]['array']['id'] = '{}-array'.format(cond)
-        my_figs[cond]['saccade']['data'] = json.dumps(go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
+        my_figs[cond]['saccade']['data'] = json.dumps(
+            go.Figure(), cls=plotly.utils.PlotlyJSONEncoder)
         my_figs[cond]['saccade']['id'] = '{}-sacc'.format(cond)
-        
-    return render_template('population_sst.html', 
-                           cond_plots = my_figs,
-                           logged_in = session['is_auth'])
-    
-    
+
+    return render_template('population_sst.html',
+                           cond_plots=my_figs,
+                           logged_in=session['is_auth'])
+
 
 ##############################
 ###### CALLBACK SECTION ######
@@ -138,7 +143,7 @@ def login_callback():
             if is_auth:
                 session['user'] = uName
     session.modified = True
-    
+
     return jsonify({'isAuth': is_auth})
 
 
@@ -175,15 +180,16 @@ def unit_update_cb():
 @app.route('/set-quality-cb', methods=['POST', 'GET'])
 def set_quality_cb():
     this_quality = request.args.get('quality')
-    
+
     if session['session'] not in session['sst_dict'].keys():
         session['sst_dict'][session['session']] = {}
     if session['unit'] not in session['sst_dict'][session['session']].keys():
         session['sst_dict'][session['session']][session['unit']] = {}
-    
-    session['sst_dict'][session['session']][session['unit']]['Quality'] = this_quality
+
+    session['sst_dict'][session['session']
+                        ][session['unit']]['Quality'] = this_quality
     session.modified = True
-    
+
     return jsonify({'success': True})
 
 
@@ -191,30 +197,34 @@ def set_quality_cb():
 def set_type_cb():
     this_vm = request.args.get('vm')
     none_checked = request.args.get('none')
-    
-    if none_checked=='true':
+
+    if none_checked == 'true':
         this_vm = 0
-    
-    session['sst_dict'][session['session']][session['unit']]['VM_Score'] = this_vm
+
+    session['sst_dict'][session['session']
+                        ][session['unit']]['VM_Score'] = this_vm
     session.modified = True
-    
+
     return jsonify({'success': True})
 
 
 @app.route('/get-scores-cb')
 def get_scores():
     try:
-        vm_val = session['sst_dict'][session['session']][session['unit']]['VM_Score']
+        vm_val = session['sst_dict'][session['session']
+                                     ][session['unit']]['VM_Score']
     except:
-        session['sst_dict'][session['session']][session['unit']]['VM_Score'] = 3
+        session['sst_dict'][session['session']
+                            ][session['unit']]['VM_Score'] = 3
         vm_val = 3
-                
+
     try:
-        qual_val = session['sst_dict'][session['session']][session['unit']]['Quality']
+        qual_val = session['sst_dict'][session['session']
+                                       ][session['unit']]['Quality']
     except:
         session['sst_dict'][session['session']][session['unit']]['Quality'] = 3
         qual_val = 3
-    
+
     return jsonify({'vm': vm_val, 'qual': qual_val})
 
 
@@ -238,10 +248,12 @@ def sst_click_parse():
     if sel_type not in session['sst_dict'][session['session']][session['unit']].keys():
         session['sst_dict'][session['session']][session['unit']][sel_type] = {}
     if plot_label not in session['sst_dict'][session['session']][session['unit']][sel_type].keys():
-        session['sst_dict'][session['session']][session['unit']][sel_type][plot_label] = []
-    session['sst_dict'][session['session']][session['unit']][sel_type][plot_label].append(click_sst)
+        session['sst_dict'][session['session']
+                            ][session['unit']][sel_type][plot_label] = []
+    session['sst_dict'][session['session']][session['unit']
+                                            ][sel_type][plot_label].append(click_sst)
     session.modified = True
-    
+
     return jsonify({'success': True})
 
 
@@ -249,10 +261,10 @@ def sst_click_parse():
 def sst_submit():
     # Save for backup security
     from random import randint
-    f_name = './tmp_dict_{}.json'.format(randint(0,1000000))
-    with open(f_name,'w') as w:
-        json.dump(session['sst_dict'],w)
-    
+    f_name = './tmp_dict_{}.json'.format(randint(0, 1000000))
+    with open(f_name, 'w') as w:
+        json.dump(session['sst_dict'], w)
+
     # Now we can do the real update
     all_sess = session['sst_dict'].keys()
     n_tot = 0
@@ -262,15 +274,16 @@ def sst_submit():
         for iu, unit in enumerate(sess_units):
             n_tot += 1
             update_dict = {}
-            update_dict['ManualTimes_'+session['user']] = session['sst_dict'][sess][unit]
+            update_dict['ManualTimes_'+session['user']
+                        ] = session['sst_dict'][sess][unit]
             update_success = UpdateUnit(sdf_coll, sess, unit, update_dict)
             if update_success:
                 n_complete += 1
             else:
-                print('Unable to update unit {} from session {}'.format(unit,sess))
+                print('Unable to update unit {} from session {}'.format(unit, sess))
     if n_tot == n_complete:
         os.remove(f_name)
-        
+
     return jsonify({'success': True})
 
 
@@ -284,51 +297,69 @@ def update_plots():
     sacc_x_min = request.args.get('sMinX')
     sacc_x_max = request.args.get('sMaxX')
     force_pull = request.args.get('forcePull')
-    
+
     # This section should be used to prevent costly database queries, but it can't do that quite yet as v_dict etc. are local variables
     # Load spike/SDF values if the values are different
-    if this_sess != session['session'] or this_unit != session['unit'] or force_pull:
-        v_dict, v_dict_sem, m_dict, m_dict_sem, unit_ssts = SpikesFromDB(this_sess, this_unit, sdf_coll, user=session['user'])
+    if this_sess != session['session'] or this_unit != session['unit'] or force_pull == 'true':
+        v_dict, v_dict_sem, m_dict, m_dict_sem, unit_ssts = SpikesFromDB(
+            this_sess, this_unit, sdf_coll, user=session['user'])
         session['session'] = this_sess
         session['unit'] = this_unit
         if this_sess not in session['sst_dict'].keys():
-            session['sst_dict'][session['session']] = {session['unit']: unit_ssts}
+            session['sst_dict'][session['session']] = {
+                session['unit']: unit_ssts}
+            session['units_loaded'].append(
+                '{}-{}'.format(session['session'], session['unit']))
         elif this_unit not in session['sst_dict'][session['session']].keys():
             session['sst_dict'][session['session']][this_unit] = unit_ssts
-        else:
-            session['sst_dict'][session['session']][session['unit']] = unit_ssts
-        
-        # If the unit gets loaded, initialize VM and Quality to 3 if they haven't been saved previously 
+            session['units_loaded'].append(
+                '{}-{}'.format(session['session'], session['unit']))
+        elif '{}-{}'.format(session['session'], session['unit']) not in session['units_loaded']:
+            session['sst_dict'][session['session']
+                                ][session['unit']] = unit_ssts
+            session['units_loaded'].append(
+                '{}-{}'.format(session['session'], session['unit']))
+        elif '{}-{}'.format(session['session'], session['unit']) in session['units_loaded']:
+            unit_ssts = session['sst_dict'][session['session']
+                                            ][session['unit']]
+
+        # If the unit gets loaded, initialize VM and Quality to 3 if they haven't been saved previously
         # (units that haven't been loaded will have no scores here and can be marked that way)
         if 'VM_Score' not in unit_ssts.keys():
-            session['sst_dict'][session['session']][session['unit']]['VM_Score'] = 3
-            session['sst_dict'][session['session']][session['unit']]['Quality'] = 3
+            session['sst_dict'][session['session']
+                                ][session['unit']]['VM_Score'] = 3
+            session['sst_dict'][session['session']
+                                ][session['unit']]['Quality'] = 3
         session.modified = True
-            
+
         # Make array-aligned figures
         for ic, cond in enumerate(plot_conds):
             # Array plots
             tmp_fig = PlotConds(v_dict, v_dict_sem, cond)
             PlotConds(v_dict, v_dict_sem, cond[0]+'0', fig=tmp_fig)
-            tmp_fig = AddVLine(tmp_fig, cond, unit_ssts,mov=False)
+            tmp_fig = AddVLine(
+                tmp_fig, cond, session['sst_dict'][session['session']][session['unit']], mov=False)
             tmp_fig.update_layout(title='{}, Array Aligned'.format(cond.upper()),
-                                  xaxis_range=[arr_x_min,arr_x_max], 
+                                  xaxis_range=[arr_x_min, arr_x_max],
                                   width=600, height=400,
-                                  spikedistance=-1,hovermode='x unified'
+                                  spikedistance=-1, hovermode='x unified'
                                   )
-            my_figs[cond]['array']['data'] = json.dumps(tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
+            my_figs[cond]['array']['data'] = json.dumps(
+                tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
             my_figs[cond]['array']['id'] = '{}-array'.format(cond)
 
             # Saccade plots
             tmp_fig = PlotConds(m_dict, m_dict_sem, cond)
             PlotConds(m_dict, m_dict_sem, cond[0]+'0', fig=tmp_fig)
-            tmp_fig = AddVLine(tmp_fig, cond, unit_ssts,mov=True)
+            tmp_fig = AddVLine(
+                tmp_fig, cond, session['sst_dict'][session['session']][session['unit']], mov=True)
             tmp_fig.update_layout(title='{}, Saccade Aligned'.format(cond.upper()),
-                                  xaxis_range=[sacc_x_min,sacc_x_max],
+                                  xaxis_range=[sacc_x_min, sacc_x_max],
                                   width=600, height=400,
-                                  spikedistance=-1,hovermode='x unified'
+                                  spikedistance=-1, hovermode='x unified'
                                   )
-            my_figs[cond]['saccade']['data'] = json.dumps(tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
+            my_figs[cond]['saccade']['data'] = json.dumps(
+                tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
             my_figs[cond]['saccade']['id'] = '{}-sacc'.format(cond)
 
         return my_figs
@@ -336,58 +367,60 @@ def update_plots():
         return {'refresh': False}
 
 
-# Make the query to the database to get the population SDF values    
-@app.route('/get-pop-plots', methods=['POST','GET'])
+# Make the query to the database to get the population SDF values
+@app.route('/get-pop-plots', methods=['POST', 'GET'])
 def get_pop_plots():
     arr_x_min = request.args.get('aMinX')
     arr_x_max = request.args.get('aMaxX')
     sacc_x_min = request.args.get('sMinX')
     sacc_x_max = request.args.get('sMaxX')
-    
+
     # Pull the population data
     pop_sdfs, unit_nhps = AllSDFs(sdf_coll, session['is_auth'])
-    
+
     # Normalize pop_sdfs for cross-unit scaling
     norm_sdfs = NormSDFs(pop_sdfs)
-    
+
     # Now generate the plots
     for ic, cond in enumerate(plot_conds):
         # Array plots
         tmp_fig = PlotPop(norm_sdfs['Vis'], cond)
         PlotPop(norm_sdfs['Vis'], cond[0]+'0', fig=tmp_fig)
         tmp_fig.update_layout(title='{}, Array Aligned'.format(cond.upper()),
-                              xaxis_range=[arr_x_min,arr_x_max], 
+                              xaxis_range=[arr_x_min, arr_x_max],
                               width=600, height=400,
-                              spikedistance=-1,hovermode='x unified'
+                              spikedistance=-1, hovermode='x unified'
                               )
-        my_figs[cond]['array']['data'] = json.dumps(tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
+        my_figs[cond]['array']['data'] = json.dumps(
+            tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
         my_figs[cond]['array']['id'] = '{}-array'.format(cond)
-        
+
         # Saccade plots
         tmp_fig = PlotPop(norm_sdfs['Mov'], cond)
         PlotPop(norm_sdfs['Mov'], cond[0]+'0', fig=tmp_fig)
         tmp_fig.update_layout(title='{}, Saccade Aligned'.format(cond.upper()),
-                              xaxis_range=[sacc_x_min,sacc_x_max], 
+                              xaxis_range=[sacc_x_min, sacc_x_max],
                               width=600, height=400,
-                              spikedistance=-1,hovermode='x unified'
+                              spikedistance=-1, hovermode='x unified'
                               )
-        my_figs[cond]['saccade']['data'] = json.dumps(tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
+        my_figs[cond]['saccade']['data'] = json.dumps(
+            tmp_fig, cls=plotly.utils.PlotlyJSONEncoder)
         my_figs[cond]['saccade']['id'] = '{}-sacc'.format(cond)
-        
+
     return my_figs
 
 
 # Request to clear sst_dict for the displayed unit
-@app.route('/clear-sst-cb', methods=['POST','GET'])
+@app.route('/clear-sst-cb', methods=['POST', 'GET'])
 def clear_ssts():
     this_sess = request.args.get('sess')
     this_unit = request.args.get('unit')
     session['sst_dict'][this_sess][this_unit] = {}
     session.modified = True
-    
+
     return jsonify({'success': True})
 
-    
+
 # Run the appP
 if __name__ == '__main__':
     app.run(debug=True)
--- utils/plotUtils.py
+++ utils/plotUtils.py
@@ -1,16 +1,17 @@
 import plotly.graph_objects as go
 import numpy as np
 
-def PltMeanStd(t, m, s, fig=None, color='rgb(0,0,0)', width = 10, name=None):
-    
+
+def PltMeanStd(t, m, s, fig=None, color='rgb(0,0,0)', width=10, name=None):
+
     if fig is None:
         fig = go.Figure()
-    
-    # Calculate upper and lower traces    
+
+    # Calculate upper and lower traces
     upper = m + s
     lower = m - s
-    t_cat = np.concatenate([t,t[::-1]])
-    y_cat = np.concatenate([upper,lower[::-1]])
+    t_cat = np.concatenate([t, t[::-1]])
+    y_cat = np.concatenate([upper, lower[::-1]])
     fig.add_trace(go.Scatter(
         x=t_cat[np.isfinite(y_cat)],
         y=y_cat[np.isfinite(y_cat)],
@@ -28,82 +29,85 @@ def PltMeanStd(t, m, s, fig=None, color='rgb(0,0,0)', width = 10, name=None):
     ))
 
 
-def PlotConds(mu_dict, sem_dict, cond, show_in=True, show_out=True, fig=None):    
-    
+def PlotConds(mu_dict, sem_dict, cond, show_in=True, show_out=True, fig=None):
+
     colors = {'hh': 'rgb(54, 54, 201)',
-                   'hl': 'rgb(0, 0, 128)',
-                   'lh': 'rgb(201, 54, 54)',
-                   'll': 'rgb(128, 0, 0)',
-                   'h0': 'rgb(54, 201, 201)',
-                   'l0': 'rgb(201, 54, 201)'}
-    
+              'hl': 'rgb(0, 0, 128)',
+              'lh': 'rgb(201, 54, 54)',
+              'll': 'rgb(128, 0, 0)',
+              'h0': 'rgb(54, 201, 201)',
+              'l0': 'rgb(201, 54, 201)'}
+
     IN_WIDTH = 3
     OUT_WIDTH = 1
-        
+
     if fig is None:
         fig = go.Figure()
-    
+
     if show_in:
-        PltMeanStd(mu_dict['t'], mu_dict[cond]['in'], sem_dict[cond]['in'], fig=fig, color=colors[cond], width=IN_WIDTH, name=cond.upper()+' IN')
+        PltMeanStd(mu_dict['t'], mu_dict[cond]['in'], sem_dict[cond]['in'],
+                   fig=fig, color=colors[cond], width=IN_WIDTH, name=cond.upper()+' IN')
     if show_out:
-        PltMeanStd(mu_dict['t'], mu_dict[cond]['out'], sem_dict[cond]['out'], fig=fig, color=colors[cond], width=OUT_WIDTH, name=cond.upper()+' OUT')
-    
+        PltMeanStd(mu_dict['t'], mu_dict[cond]['out'], sem_dict[cond]['out'],
+                   fig=fig, color=colors[cond], width=OUT_WIDTH, name=cond.upper()+' OUT')
+
     return fig
 
 
-def PlotPop(pop_sdfs, cond, show_in=True, show_out=True, fig=None):    
-    
+def PlotPop(pop_sdfs, cond, show_in=True, show_out=True, fig=None):
+
     import numpy as np
-    
+
     colors = {'hh': 'rgb(54, 54, 201)',
-                   'hl': 'rgb(0, 0, 128)',
-                   'lh': 'rgb(201, 54, 54)',
-                   'll': 'rgb(128, 0, 0)',
-                   'h0': 'rgb(54, 201, 201)',
-                   'l0': 'rgb(201, 54, 201)'}
-    
+              'hl': 'rgb(0, 0, 128)',
+              'lh': 'rgb(201, 54, 54)',
+              'll': 'rgb(128, 0, 0)',
+              'h0': 'rgb(54, 201, 201)',
+              'l0': 'rgb(201, 54, 201)'}
+
     IN_WIDTH = 3
     OUT_WIDTH = 1
 
     if fig is None:
         fig = go.Figure()
-    
+
     if show_in:
         PltMeanStd(pop_sdfs['Times'],
-                   np.nanmean(pop_sdfs[cond.upper()]['in'],axis=0),
-                   np.nanstd(pop_sdfs[cond.upper()]['in'],axis=0)/np.sqrt(pop_sdfs[cond.upper()]['in'].shape[0]),
+                   np.nanmean(pop_sdfs[cond.upper()]['in'], axis=0),
+                   np.nanstd(pop_sdfs[cond.upper()]['in'], axis=0) /
+                   np.sqrt(pop_sdfs[cond.upper()]['in'].shape[0]),
                    fig=fig, color=colors[cond], width=IN_WIDTH, name=cond.upper()+' IN')
     if show_out:
         PltMeanStd(pop_sdfs['Times'],
-                   np.nanmean(pop_sdfs[cond.upper()]['out'],axis=0),
-                   np.nanstd(pop_sdfs[cond.upper()]['out'],axis=0)/np.sqrt(pop_sdfs[cond.upper()]['out'].shape[0]),
+                   np.nanmean(pop_sdfs[cond.upper()]['out'], axis=0),
+                   np.nanstd(pop_sdfs[cond.upper()]['out'], axis=0) /
+                   np.sqrt(pop_sdfs[cond.upper()]['out'].shape[0]),
                    fig=fig, color=colors[cond], width=OUT_WIDTH, name=cond.upper()+' OUT')
-    
+
     return fig
 
 
 def GetYRange(fig):
     fig_data = fig.data
-    
-    min_y = min(min(list(map(lambda x: min(x.y), fig_data))),0.)
+
+    min_y = min(min(list(map(lambda x: min(x.y), fig_data))), 0.)
     max_y = max(list(map(lambda x: max(x.y), fig_data)))
     range_y = max_y-min_y
-    
+
     return [min_y-(.05*range_y), max_y+(.05*range_y)]
 
 
 def AddVLine(fig, cond, sst_dict, mov=False):
     # Define selection types
-    sel_types = ['sst','cdt','msst']
-    sel_colors = {'sst': 'rgb(54,201,54)', 'cdt': 'rgb(54, 201, 201)', 'ngsst': 'rgb(230, 147, 23)', 'outcdt': 'rgb(201, 201, 54)', 'unk': 'rgb(0,0,0)'};
+    sel_types = ['sst', 'cdt', 'msst', 'mcdt']
+    sel_colors = {'sst': 'rgb(54,201,54)', 'cdt': 'rgb(54, 201, 201)',
+                  'ngsst': 'rgb(230, 147, 23)', 'outcdt': 'rgb(201, 201, 54)', 'unk': 'rgb(0,0,0)'}
     # Get Y lim
     y_range = GetYRange(fig)
-    
+
+    if mov:
+        cond = 'm'+cond
     for it, sel in enumerate(sel_types):
-        if sel[0]=='m' and mov==False:
-            continue
-        if sel[0]!='m' and mov==True:
-            continue
         if sel in sst_dict.keys() and cond in sst_dict[sel].keys():
             these_ssts = sst_dict[sel][cond]
             if type(these_ssts) is str:
@@ -111,14 +115,14 @@ def AddVLine(fig, cond, sst_dict, mov=False):
             for ii, t_val in enumerate(these_ssts):
                 try:
                     fig.add_trace(go.Scatter(
-                    x=[int(t_val),int(t_val)],
-                    y=y_range,
-                    line=dict(color=sel_colors[sel]),
-                    name=sel+'_'+str(ii),
-                    visible=True
+                        x=[int(t_val), int(t_val)],
+                        y=y_range,
+                        line=dict(color=sel_colors[sel]),
+                        name=sel+'_'+str(ii),
+                        visible=True
                     ))
                 except:
                     pass
     fig.update_layout(yaxis_range=y_range)
-    
+
     return fig
